2bbcd3c6c1ae03be325818b1b255cd5b5f3ce1af
Lou <lou@Lous-iMac.home>
2017-08-14 18:01:04 -0400

    added Post model and newsfeed method to User

---
+++ application/posts/models.py
@@ -0,0 +1,17 @@
+import datetime
+from application import db
+
+__all__ = ['Post']
+
+class Post(db.Model):
+
+	id = db.Column(db.Integer(), primary_key=True)
+
+	content = db.Column(db.Text())
+
+	created_on = db.Column(db.DateTime, defatul=datetime.datetime.utcnow, index=True)
+
+	user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return "<Post %r>" % self.body
