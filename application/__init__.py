8d4996fe0e79af07a71f2cc809f6a8eb60e20122
Lou <lou@Lous-iMac.home>
2017-08-14 17:27:16 -0400

    basic setup

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+# initalize db extension without configuring
+db = SQLAlchemy()
+flask_bcrypt = Bcrypt()
+
 
 def create_app(config=None):
 	app = Flask(__name__)
 
 	if config is not None:
 		app.config.from_object(config)
+
+	# add db and bcrypt to app
+	db.init_app(app)
+	flask_bcrypt.init_app(app)
 
 	return app
